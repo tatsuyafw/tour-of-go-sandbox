a9efbd3a124db1664100dc2a27684f6b803f2cdc
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2016-05-21 22:05:10 +0900

    Improve

--- concurrency/exercise-equivalent-binary-trees.go
+++ concurrency/exercise-equivalent-binary-trees.go
@@ -5,16 +5,20 @@ import (
 	"golang.org/x/tour/tree"
 )
 
+func WalkImpl(t *tree.Tree, ch chan int) {
+	if t == nil {
+		return
+	}
+	WalkImpl(t.Left, ch)
+	ch <- t.Value
+	WalkImpl(t.Right, ch)
+}
+
 // Walk wals the tree t sending all values
 // from the tree to the cannel ch.
 func Walk(t *tree.Tree, ch chan int) {
-	if t.Left != nil {
-		Walk(t.Left, ch)
-	}
-	ch <- t.Value
-	if t.Right != nil {
-		Walk(t.Right, ch)
-	}
+	WalkImpl(t, ch)
+	close(ch)
 }
 
 // Same determines whether the trees
@@ -24,15 +28,26 @@ func Same(t1, t2 *tree.Tree) bool {
 	ch2 := make(chan int)
 	go Walk(t1, ch1)
 	go Walk(t2, ch2)
-	for i := 0; i < 10; i++ {
-		if <-ch1 != <-ch2 {
+	for {
+		v1, ok1 := <-ch1
+		v2, ok2 := <-ch2
+		if v1 != v2 {
 			return false
+		}
+		if !ok1 && !ok2 {
+			break
 		}
 	}
 	return true
 }
 
 func main() {
+	ch := make(chan int)
+	go Walk(tree.New(1), ch)
+	for value := range ch {
+		fmt.Printf("value = %v\n", value)
+	}
+
 	fmt.Println(Same(tree.New(1), tree.New(1)))
 	fmt.Println(Same(tree.New(1), tree.New(2)))
 }
